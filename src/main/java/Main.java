777d171ed11604b8fb5cfd004ec11ec1d86b708c
Uday <iamudayhegde@gmail.com>
2025-03-15 08:37:58 +0530

    Added 200 OK

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,33 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Logger;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-     try {
-       ServerSocket serverSocket = new ServerSocket(4221);
+    private static final Logger log = Logger.getLogger(Main.class.getName());
 
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
 
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+    public static void main(String[] args) {
+        System.out.println("Logs from your program will appear here!");
+        try {
+            ServerSocket serverSocket = new ServerSocket(4221);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                log.info("Accepted connection");
+                System.out.println("accepted new connection");
+                OutputStream outputStream = clientSocket.getOutputStream();
+                String response = "HTTP/1.1 200 OK\r\n\r\n";
+                outputStream.write(response.getBytes());
+                outputStream.flush();
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
