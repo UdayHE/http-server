c075cefce860164fc07631be59250f83c5ad9172
Uday <iamudayhegde@gmail.com>
2025-03-15 11:43:31 +0530

    Adds logic to send 200 & 404 http reponse code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static constant.Constant.ROOT;
+import static constant.Constant.SPACE;
 
 public class Main {
 
@@ -10,7 +16,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Logs from your program will appear here!");
+        log.info("HTTP Server Started.");
         try {
             ServerSocket serverSocket = new ServerSocket(4221);
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
@@ -19,10 +25,25 @@ public class Main {
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 log.info("Accepted connection");
-                System.out.println("accepted new connection");
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 OutputStream outputStream = clientSocket.getOutputStream();
-                String response = "HTTP/1.1 200 OK\r\n\r\n";
-                outputStream.write(response.getBytes());
+
+                String requestLine = bufferedReader.readLine();
+                log.log(Level.INFO, "Request:{0}", requestLine);
+
+                if (requestLine != null && !requestLine.isBlank()) {
+                    String[] requestParts = requestLine.split(SPACE);
+                    if (requestParts.length > 2) {
+                        String path = requestParts[1];
+                        String response;
+                        if (ROOT.equals(path))
+                            response = "HTTP/1.1 200 OK\r\n\r\n";
+                        else
+                            response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                        outputStream.write(response.getBytes());
+                        outputStream.flush();
+                    }
+                }
                 outputStream.flush();
                 clientSocket.close();
             }
@@ -31,3 +52,4 @@ public class Main {
         }
     }
 }
+
---
+++ src/main/java/constant/Constant.java
@@ -0,0 +1,9 @@
+package constant;
+
+public class Constant {
+
+    private Constant() {}
+
+    public static final String SPACE = " ";
+    public static final String ROOT = "/";
+}
