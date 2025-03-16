9fe284148f868276185f42b2c27887ad4f27bc61
Uday <iamudayhegde@gmail.com>
2025-03-16 19:37:21 +0530

    Added support for Accept-Encoding and Content-Encoding

--- src/main/java/httpserver/handler/implementation/UserAgent.java
+++ src/main/java/httpserver/handler/implementation/UserAgent.java
@@ -3,20 +3,15 @@ package httpserver.handler.implementation;
 import httpserver.dto.Request;
 import httpserver.handler.RequestHandler;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
-import static httpserver.constant.Constant.EMPTY;
-import static httpserver.constant.Constant.USER_AGENT;
-
 public class UserAgent implements RequestHandler {
 
     @Override
     public void handle(Request request) throws IOException {
-        BufferedReader reader = request.getReader();
         OutputStream outputStream = request.getOutputStream();
         Map<String, String> headers = request.getHeaders();
 
@@ -31,15 +26,4 @@ public class UserAgent implements RequestHandler {
         outputStream.flush();
     }
 
-    private String getUserAgent(BufferedReader reader) throws IOException {
-        String userAgent = EMPTY;
-        String line;
-        while ((line = reader.readLine()) != null && !line.isEmpty()) {
-            if (line.startsWith(USER_AGENT)) {
-                userAgent = line.substring(12).trim();
-                break;
-            }
-        }
-        return userAgent;
-    }
 }
