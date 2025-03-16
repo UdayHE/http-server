08b1b9c35558186f7aeee973c4cdfde8f1016853
Uday <iamudayhegde@gmail.com>
2025-03-16 19:51:50 +0530

    Added support for Multiple compression schemes

--- src/main/java/httpserver/handler/implementation/Echo.java
+++ src/main/java/httpserver/handler/implementation/Echo.java
@@ -7,6 +7,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.zip.GZIPOutputStream;
 
@@ -19,22 +20,36 @@ public class Echo implements RequestHandler {
         Map<String, String> headers = request.getHeaders();
 
         String message = path.substring(6);
-        boolean useGzip = "gzip".equals(headers.get("Accept-Encoding"));
+//        boolean useGzip = "gzip".equals(headers.get("Accept-Encoding"));
+//
+//        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+//        OutputStream responseStream = useGzip ? new GZIPOutputStream(byteStream) : byteStream;
+//        responseStream.write(message.getBytes(StandardCharsets.UTF_8));
+//        responseStream.close();
+//
+//        byte[] responseBody = byteStream.toByteArray();
+//
+//        String responseHeaders = "HTTP/1.1 200 OK\r\n" +
+//                "Content-Type: text/plain\r\n" +
+//                (useGzip ? "Content-Encoding: gzip\r\n" : "") +
+//                "Content-Length: " + responseBody.length + "\r\n\r\n";
+//
+//        outputStream.write(responseHeaders.getBytes());
+//        outputStream.write(responseBody, 0, responseBody.length);
+//        outputStream.flush();
 
-        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-        OutputStream responseStream = useGzip ? new GZIPOutputStream(byteStream) : byteStream;
-        responseStream.write(message.getBytes(StandardCharsets.UTF_8));
-        responseStream.close();
-
-        byte[] responseBody = byteStream.toByteArray();
+        String acceptEncoding = headers.getOrDefault("Accept-Encoding", "");
+        boolean useGzip = Arrays.stream(acceptEncoding.split(", "))
+                .map(String::trim)
+                .anyMatch(enc -> "gzip".equalsIgnoreCase(enc));
 
         String responseHeaders = "HTTP/1.1 200 OK\r\n" +
                 "Content-Type: text/plain\r\n" +
                 (useGzip ? "Content-Encoding: gzip\r\n" : "") +
-                "Content-Length: " + responseBody.length + "\r\n\r\n";
+                "Content-Length: " + message.length() + "\r\n\r\n";
 
-        outputStream.write(responseHeaders.getBytes());
-        outputStream.write(responseBody, 0, responseBody.length);
+        outputStream.write(responseHeaders.getBytes(StandardCharsets.UTF_8));
+        outputStream.write(message.getBytes(StandardCharsets.UTF_8));
         outputStream.flush();
     }
 }
