25b1d51363a0787cb14b6b226413225a3e50563e
Uday <iamudayhegde@gmail.com>
2025-03-16 19:54:37 +0530

    Added support for Multiple compression schemes

--- src/main/java/httpserver/handler/implementation/Echo.java
+++ src/main/java/httpserver/handler/implementation/Echo.java
@@ -3,13 +3,13 @@ package httpserver.handler.implementation;
 import httpserver.dto.Request;
 import httpserver.handler.RequestHandler;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.zip.GZIPOutputStream;
+
+import static httpserver.constant.Constant.*;
 
 public class Echo implements RequestHandler {
 
@@ -20,28 +20,10 @@ public class Echo implements RequestHandler {
         Map<String, String> headers = request.getHeaders();
 
         String message = path.substring(6);
-//        boolean useGzip = "gzip".equals(headers.get("Accept-Encoding"));
-//
-//        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-//        OutputStream responseStream = useGzip ? new GZIPOutputStream(byteStream) : byteStream;
-//        responseStream.write(message.getBytes(StandardCharsets.UTF_8));
-//        responseStream.close();
-//
-//        byte[] responseBody = byteStream.toByteArray();
-//
-//        String responseHeaders = "HTTP/1.1 200 OK\r\n" +
-//                "Content-Type: text/plain\r\n" +
-//                (useGzip ? "Content-Encoding: gzip\r\n" : "") +
-//                "Content-Length: " + responseBody.length + "\r\n\r\n";
-//
-//        outputStream.write(responseHeaders.getBytes());
-//        outputStream.write(responseBody, 0, responseBody.length);
-//        outputStream.flush();
-
-        String acceptEncoding = headers.getOrDefault("Accept-Encoding", "");
-        boolean useGzip = Arrays.stream(acceptEncoding.split(", "))
+        String acceptEncoding = headers.getOrDefault(ACCEPT_ENCODING, EMPTY);
+        boolean useGzip = Arrays.stream(acceptEncoding.split(COMMA))
                 .map(String::trim)
-                .anyMatch(enc -> "gzip".equalsIgnoreCase(enc));
+                .anyMatch(GZIP::equalsIgnoreCase);
 
         String responseHeaders = "HTTP/1.1 200 OK\r\n" +
                 "Content-Type: text/plain\r\n" +
