18ff973ed8eb44d6a3b5cef4b75673fe23168a24
Uday <iamudayhegde@gmail.com>
2025-03-17 11:38:07 +0530

    Removed Unused imports

--- src/main/java/httpserver/handler/factory/FileHandlerStrategyFactory.java
+++ src/main/java/httpserver/handler/factory/FileHandlerStrategyFactory.java
@@ -6,22 +6,32 @@ import httpserver.handler.implementation.strategy.Get;
 import httpserver.handler.implementation.strategy.Post;
 import httpserver.helper.RequestResponseHelper;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import static httpserver.enums.HttpMethod.GET;
 import static httpserver.enums.HttpMethod.POST;
+import static java.util.Objects.nonNull;
 
 public class FileHandlerStrategyFactory {
 
-    private final Map<HttpMethod, FileHandlerStrategy> strategies = new ConcurrentHashMap<>();
+    private final Map<HttpMethod, FileHandlerStrategy> strategies = new HashMap<>();
 
     public FileHandlerStrategyFactory(RequestResponseHelper requestResponseHelper) {
-        this.strategies.put(GET, new Get(requestResponseHelper));
-        this.strategies.put(POST, new Post(requestResponseHelper));
+        if(nonNull(requestResponseHelper)) {
+            this.strategies.put(GET, new Get(requestResponseHelper));
+            this.strategies.put(POST, new Post(requestResponseHelper));
+        }
     }
 
     public FileHandlerStrategy getStrategy(HttpMethod method) {
-        return this.strategies.get(method);
+        if (method == null) {
+            throw new IllegalArgumentException("HTTP method cannot be null");
+        }
+        FileHandlerStrategy strategy = this.strategies.get(method);
+        if (strategy == null) {
+            throw new UnsupportedOperationException("Unsupported HTTP method: " + method);
+        }
+        return strategy;
     }
 }
