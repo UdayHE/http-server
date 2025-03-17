ec4ad62af6c29d1b62e644d4638c467996b7c41b
Uday <iamudayhegde@gmail.com>
2025-03-17 11:28:00 +0530

    Added RequestResponseHelper

--- src/main/java/httpserver/handler/factory/FileHandlerStrategyFactory.java
+++ src/main/java/httpserver/handler/factory/FileHandlerStrategyFactory.java
@@ -8,27 +8,17 @@ import httpserver.helper.RequestResponseHelper;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import static httpserver.enums.HttpMethod.GET;
 import static httpserver.enums.HttpMethod.POST;
 
 public class FileHandlerStrategyFactory {
 
-    private static final Logger log = Logger.getLogger(FileHandlerStrategyFactory.class.getName());
-
     private final Map<HttpMethod, FileHandlerStrategy> strategies = new ConcurrentHashMap<>();
 
     public FileHandlerStrategyFactory(RequestResponseHelper requestResponseHelper) {
-        try {
-            synchronized (FileHandlerStrategyFactory.class) {
-                this.strategies.put(GET, new Get(requestResponseHelper));
-                this.strategies.put(POST, new Post(requestResponseHelper));
-            }
-        } catch (Exception e) {
-            log.log(Level.SEVERE, "Exception in FileHandlerStrategyFactory()..{0}", e.getMessage());
-        }
+        this.strategies.put(GET, new Get(requestResponseHelper));
+        this.strategies.put(POST, new Post(requestResponseHelper));
     }
 
     public FileHandlerStrategy getStrategy(HttpMethod method) {
