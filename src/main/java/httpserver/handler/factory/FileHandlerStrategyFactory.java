c5aef876561bea2e62072f9c88b6f263c8c53562
Uday <iamudayhegde@gmail.com>
2025-03-17 11:38:36 +0530

    Refactor

--- src/main/java/httpserver/handler/factory/FileHandlerStrategyFactory.java
+++ src/main/java/httpserver/handler/factory/FileHandlerStrategyFactory.java
@@ -18,20 +18,18 @@ public class FileHandlerStrategyFactory {
     private final Map<HttpMethod, FileHandlerStrategy> strategies = new HashMap<>();
 
     public FileHandlerStrategyFactory(RequestResponseHelper requestResponseHelper) {
-        if(nonNull(requestResponseHelper)) {
+        if (nonNull(requestResponseHelper)) {
             this.strategies.put(GET, new Get(requestResponseHelper));
             this.strategies.put(POST, new Post(requestResponseHelper));
         }
     }
 
     public FileHandlerStrategy getStrategy(HttpMethod method) {
-        if (method == null) {
+        if (method == null)
             throw new IllegalArgumentException("HTTP method cannot be null");
-        }
         FileHandlerStrategy strategy = this.strategies.get(method);
-        if (strategy == null) {
+        if (strategy == null)
             throw new UnsupportedOperationException("Unsupported HTTP method: " + method);
-        }
         return strategy;
     }
 }
